610c2c939743fcb54c0096de6847ab95fc373236
Alex <alexander.christianson@outlook.com>
2024-11-05 15:00:25 +0100

    Component for choosing a timeslot for booking

--- Restaurant-React/src/Components/ChooseTime.jsx
+++ Restaurant-React/src/Components/ChooseTime.jsx
@@ -1,17 +1,20 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
+import { isEmptyObject } from "jquery";
 
 
 export default function ChooseTime({selectedDate, numberOfGuests, sendDataToReservation}){
     const [timeSlots, setTimeSlots] = useState([]);
-    const [selectedTimeSlot, setSelectedTimeSlot] = useState([null]);
+    const [selectedTimeSlot, setSelectedTimeSlot] = useState(null);
 
+    //Checks if you number of guests and date is chosen, and updates if another date or nr of guests is chosen
     useEffect(() => {
         if (selectedDate && numberOfGuests) {
             getTimeSlots();
         }
     }, [selectedDate, numberOfGuests]);
 
+    //Gets the avialable time-slots for a specific day
     async function getTimeSlots(){
         try {
             const response = await axios.get(`https://localhost:7190/api/Table/availableTables`, {
@@ -26,10 +29,27 @@ export default function ChooseTime({selectedDate, numberOfGuests, sendDataToRese
         }
     }
 
-    const handleOnClick = (slot) => {
+    /* //Checks if a timeslot is chosen before allowing parent to render CreateReservation
+    useEffect(() => {
+        if(selectedTimeSlot){
+            setCanRenderCreateReservation(true)
+        }
+    }, [selectedTimeSlot]); */
+
+    //Sends the data back to parent if a time-slot is chosen
+    function handleCreateResButtonOnClick(){
+        if(selectedTimeSlot){
+            sendDataToReservation(selectedTimeSlot, numberOfGuests); // Send the data back to parent
+        } else {
+            alert("choose a timeslot!")
+        }
+    }
+
+    //Sets the time-slot 
+    //Maybe skip and just do select on the button instant
+    const handleSlotSelectionButtonOnClick = (slot) => {
         if (slot.isAvailable) {
             setSelectedTimeSlot(slot);
-            sendDataToReservation(slot.slotStart); // Send the data back to parent
         }
     };
 
@@ -38,14 +58,17 @@ export default function ChooseTime({selectedDate, numberOfGuests, sendDataToRese
             <h3>Available Time Slots</h3>
             <div className="d-flex flex-wrap">
                 {timeSlots.map((slot, index) => (
-                    <button
+                    <div>
+                        <button
                         key={index}
                         className={`btn ${slot.isAvailable ? 'btn-success' : 'btn-danger'} m-1`}
                         disabled={!slot.isAvailable}
-                        onClick={() => slot.isAvailable && handleOnClick(slot)}
+                        onClick={() => slot.isAvailable && handleSlotSelectionButtonOnClick(slot)}
                     >
-                        {new Date(slot.slotStart).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'UTC' })} - {new Date(slot.slotEnd).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'UTC' })}
-                    </button>
+                            {new Date(slot.slotStart).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'UTC' })} - {new Date(slot.slotEnd).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'UTC' })}
+                        </button>
+                        
+                    </div>
                 ))}
             </div>
             {selectedTimeSlot && (
@@ -54,9 +77,16 @@ export default function ChooseTime({selectedDate, numberOfGuests, sendDataToRese
                     <div className="border rounded hover-none">
                         {new Date(selectedTimeSlot.slotStart).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'UTC' })} - {new Date(selectedTimeSlot.slotEnd).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'UTC' })}
                     </div>
+                </div>)}
+            
+                <div>
+                    <button className="btn btn-info mt-3" onClick={handleCreateResButtonOnClick}>
+                        Create Reservation
+                    </button>
                 </div>
-            )}
         </div>
+
+        
     )
 }
 
