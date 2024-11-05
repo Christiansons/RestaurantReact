3ab44975df0c3bd515756e72064a048546914bec
Alex <alexander.christianson@outlook.com>
2024-11-05 14:59:18 +0100

    added routes

--- Restaurant-React/src/App.jsx
+++ Restaurant-React/src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
 import Reservation from './Components/Reservation'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import BookingConfirmed from './Components/BookingConfirmed'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Reservation/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Reservation/>}/>
+          <Route path='/booking-confirmed' element={<BookingConfirmed/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
